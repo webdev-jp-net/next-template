449e1744322ea8687f5d7ca3bf523b68c75d998e
haitani <haitani@tam-tam.co.jp>
2020-06-22 16:21:44 +0900

    Fix: SyntaxError 修正

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,23 @@
-self.addEventListener('install', function(e) (
-  console.log('ServicrWorker Install');
-), false);
+self.addEventListener(
+  'install',
+  function (e) {
+    console.log('ServicrWorker Install');
+  },
+  false
+);
 
-self.addEventListener('activate', function(e) (
-  console.log('ServicrWorker Activate');
-), false);
+self.addEventListener(
+  'activate',
+  function (e) {
+    console.log('ServicrWorker Activate');
+  },
+  false
+);
 
-self.addEventListener('fetch', function(e) (
-  console.log('ServicrWorker Fetch');
-), false);
-
+self.addEventListener(
+  'fetch',
+  function (e) {
+    console.log('ServicrWorker Fetch');
+  },
+  false
+);
