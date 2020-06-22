5ca067db00281ff87d194969e99011d27cbe97b6
haitani <haitani@tam-tam.co.jp>
2020-06-22 15:29:07 +0900

    Add: PWA簡易対応

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,10 +13,13 @@ export default class MyDocument extends Document {
           <meta name="apple-mobile-web-app-status-bar-style" content="default"></meta>
           <meta name="apple-mobile-web-app-title" content="NEXT-TEMPLATE"></meta>
           <meta name="format-detection" content="telephone=no" />
+          <link rel="apple-touch-icon" href="img/ico_152x152.png"></link>
+          <link rel="manifest" href="manifest.json"></link>
         </Head>
         <body>
           <Main />
           <NextScript />
+          <script src="script.js"></script>
         </body>
       </html>
     );
---
+++ public/script.js
@@ -0,0 +1,14 @@
+console.log('serviceWorker' in navigator);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('sw.js').then(
+      function (sw) {
+        console.log('ServiceWorker scope: ', sw.scope);
+      },
+      function (err) {
+        console.log('ServiceWorker failed: ', err);
+      }
+    );
+  });
+}
