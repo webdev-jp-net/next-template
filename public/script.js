df7b491d8f2698e6164a62a79ab2e35877dfa0a3
haitani <haitani@tam-tam.co.jp>
2020-06-22 15:58:52 +0900

    Fix : デプロイコマンドの誤りを修正

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-console.log('serviceWorker' in navigator);
-
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker.register('sw.js').then(
